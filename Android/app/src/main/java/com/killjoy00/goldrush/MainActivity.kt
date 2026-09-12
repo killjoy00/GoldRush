f760e361982d9a77e40dfcccb5f2e23e17c56a69
killjoy00 <killjoy00@yahoo.com>
2026-09-12 15:57:53 -0500

    Observe rating eligibility without API 37 lifecycle dependency

--- Android/app/src/main/java/com/killjoy00/goldrush/MainActivity.kt
+++ Android/app/src/main/java/com/killjoy00/goldrush/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.lifecycleScope
 import com.killjoy00.goldrush.career.CareerStats
 import com.killjoy00.goldrush.career.CareerStatsRepository
 import com.killjoy00.goldrush.review.RatingsPrompt
 import com.killjoy00.goldrush.ui.GoldRushApp
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val activityScope = MainScope()
     private val careerRepository by lazy { CareerStatsRepository(applicationContext) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +24,13 @@ class MainActivity : ComponentActivity() {
         setContent { GoldRushApp() }
     }
 
+    override fun onDestroy() {
+        activityScope.cancel()
+        super.onDestroy()
+    }
+
     private fun observeCareerForRatingsPrompt() {
-        lifecycleScope.launch {
+        activityScope.launch {
             var previous: CareerStats? = null
             careerRepository.stats.collect { current ->
                 val prior = previous
