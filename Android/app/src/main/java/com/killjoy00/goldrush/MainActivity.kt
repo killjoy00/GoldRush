ea60a41db24a98afd531de929ca6889c08c8a596
killjoy00 <killjoy00@yahoo.com>
2026-09-12 15:01:20 -0500

    Host Play in-app review flow

--- Android/app/src/main/java/com/killjoy00/goldrush/MainActivity.kt
+++ Android/app/src/main/java/com/killjoy00/goldrush/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.killjoy00.goldrush.review.PlayReviewPromptHost
 import com.killjoy00.goldrush.ui.GoldRushApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { GoldRushApp() }
+        setContent {
+            GoldRushApp()
+            PlayReviewPromptHost(this@MainActivity)
+        }
     }
 }
