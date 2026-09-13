48844be987fbec7e177dd20e4f8d0a491c0a5758
killjoy00 <killjoy00@yahoo.com>
2026-09-13 14:30:23 -0500

    Gate banners on completed Mobile Ads init

--- Android/app/src/main/java/com/killjoy00/goldrush/ui/MonetizedGoldRushApp.kt
+++ Android/app/src/main/java/com/killjoy00/goldrush/ui/MonetizedGoldRushApp.kt
@@ -18,9 +18,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -53,6 +53,7 @@ fun MonetizedGoldRushApp(adsConsentManager: AdsConsentManager) {
     }
 
     val canShowAds = adsConsentState.canRequestAds &&
+        adsConsentState.mobileAdsReady &&
         !removeAdsState.isPurchased &&
         (BuildConfig.DEBUG || removeAdsState.isEntitlementResolved)
     val canShowFooter = activeSession == null && !showRemoveAds
